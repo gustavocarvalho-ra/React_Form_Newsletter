ef64ea8752c2064c7aa974c964fd600aab0a754f
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-15 15:50:33 -0300

    at4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,29 @@
 const Form = () => {
   return (
-    <div>Form</div>
+    <form className="flex flex-col gap-3">
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">Nome</label>
+        <input 
+          type="text" 
+          placeholder="Digite o seu nome" 
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"/>
+      </div>
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">E-mail</label>
+        <input 
+          type="email" 
+          placeholder="Insira o seu melhor e-mail" 
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"/>
+      </div>
+      <div className="flex flex-col">
+        <a href="#" className="">leia os termos</a>
+        <div>
+          <input type="checkbox"/>
+          <label className="text-sm" htmlFor="agree">Concordo com os termos</label>
+        </div>
+      </div>
+      <button>Cadastrar</button>
+    </form>
   )
 }
 
