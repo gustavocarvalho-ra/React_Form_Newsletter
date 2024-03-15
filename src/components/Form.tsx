820b922715975f9cfddc83df7096e3a290641bed
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-15 16:04:11 -0300

    at5

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,16 @@
+import { useState, FormEvent } from "react";
+import { User } from '../types/User';
+
+
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  }
+
   return (
     <form className="flex flex-col gap-3">
       <div className="flex flex-col">
@@ -16,13 +28,13 @@ const Form = () => {
           className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"/>
       </div>
       <div className="flex flex-col">
-        <a href="#" className="">leia os termos</a>
-        <div>
+        <a href="#" className="text-xs underline mb-2">Leia os termos</a>
+        <div className="flex gap-2 items-center">
           <input type="checkbox"/>
           <label className="text-sm" htmlFor="agree">Concordo com os termos</label>
         </div>
       </div>
-      <button>Cadastrar</button>
+      <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
     </form>
   )
 }
